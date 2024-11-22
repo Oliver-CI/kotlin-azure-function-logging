93d9b44332fbd2de53cf07316438e329e707ee89
Mart-IS <mart.deroos@infosupport.com>
2024-11-22 17:53:05 +0100

    test another span

--- src/main/kotlin/com/kotlin/azure/function/functions/LogFunction.kt
+++ src/main/kotlin/com/kotlin/azure/function/functions/LogFunction.kt
@@ -6,12 +6,16 @@ import com.microsoft.azure.functions.HttpRequestMessage
 import com.microsoft.azure.functions.annotation.AuthorizationLevel
 import com.microsoft.azure.functions.annotation.FunctionName
 import com.microsoft.azure.functions.annotation.HttpTrigger
-import io.opentelemetry.javaagent.shaded.io.opentelemetry.api.common.AttributeKey
-import io.opentelemetry.javaagent.shaded.io.opentelemetry.api.trace.Span
+import io.opentelemetry.api.GlobalOpenTelemetry
+import io.opentelemetry.api.common.AttributeKey
+import io.opentelemetry.api.trace.Tracer
 import mu.KotlinLogging
 import org.springframework.stereotype.Component
 
+
 val kLogger = KotlinLogging.logger {}
+
+val tracer: Tracer = GlobalOpenTelemetry.getTracer("com.kotlin.azure.function.functions")
 
 @Component
 class LogFunction {
@@ -25,15 +29,21 @@ class LogFunction {
         ) request: HttpRequestMessage<String>,
         context: ExecutionContext
     ) {
-        val span = Span.current()
-        val attributeKey = AttributeKey.stringKey("custom-string-attribute")
-        span.setAttribute(attributeKey, "custom-string-value")
-        span.setAttribute("another-custom-attribute", 42)
-        val message = request.body
-        context.logger.fine("CONTEXT LOGGER fine: $message")
-        context.logger.info { "CONTEXT LOGGER info: $message" }
-        context.logger.warning("CONTEXT LOGGER warning: $message")
-        context.logger.severe("CONTEXT LOGGER severe: $message")
-        kLogger.info { "KOTLIN LOGGER info: $message" }
+        val span = tracer.spanBuilder("context-logger-spanned").startSpan()
+        try {
+            span.makeCurrent().use {
+                val attributeKey = AttributeKey.stringKey("custom-string-attribute")
+                span.setAttribute(attributeKey, "custom-string-value")
+                span.setAttribute("another-custom-attribute", 42)
+                val message = request.body
+                context.logger.fine("CONTEXT LOGGER fine: $message")
+                context.logger.info { "CONTEXT LOGGER info: $message" }
+                context.logger.warning("CONTEXT LOGGER warning: $message")
+                context.logger.severe("CONTEXT LOGGER severe: $message")
+                kLogger.info { "KOTLIN LOGGER info: $message" }
+            }
+        } finally {
+            span.end()
+        }
     }
 }
