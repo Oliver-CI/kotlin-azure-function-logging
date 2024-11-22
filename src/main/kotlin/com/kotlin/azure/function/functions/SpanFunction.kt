d4536b2ea9be5d4a0af656787bac60493bfa981a
Mart-IS <mart.deroos@infosupport.com>
2024-11-22 14:57:53 +0100

    add span function test

---
+++ src/main/kotlin/com/kotlin/azure/function/functions/SpanFunction.kt
@@ -0,0 +1,44 @@
+package com.kotlin.azure.function.functions
+
+import com.azure.core.util.logging.ClientLogger
+import com.kotlin.azure.function.logger.LoggerFactory
+import com.microsoft.azure.functions.ExecutionContext
+import com.microsoft.azure.functions.HttpMethod
+import com.microsoft.azure.functions.HttpRequestMessage
+import com.microsoft.azure.functions.annotation.AuthorizationLevel
+import com.microsoft.azure.functions.annotation.FunctionName
+import com.microsoft.azure.functions.annotation.HttpTrigger
+import io.opentelemetry.api.common.AttributeKey
+import io.opentelemetry.api.trace.Span
+import mu.KotlinLogging
+import org.springframework.stereotype.Component
+
+@Component
+class SpanFunction {
+    @FunctionName("LoggingWithSpan")
+    fun handleClientLogger(
+        @HttpTrigger(
+            name = "request",
+            methods = [HttpMethod.POST],
+            authLevel = AuthorizationLevel.ANONYMOUS,
+            route = "spanned-loggers"
+        ) request: HttpRequestMessage<String>,
+        context: ExecutionContext
+    ) {
+        val message = request.body
+        val clientLogger = ClientLogger(this::class.java)
+        val contextLogger = context.logger
+        val kotlinLogger = KotlinLogging.logger {}
+        val customLogger = LoggerFactory.getLogger(this::class)
+
+        val span = Span.current()
+        val attributeKey = AttributeKey.stringKey("custom-string-attribute")
+        span.setAttribute(attributeKey, "custom-string-value")
+        span.setAttribute("another-custom-attribute", 42)
+
+        clientLogger.warning("client-logger warning: $message")
+        contextLogger.warning("context-logger warning: $message")
+        kotlinLogger.warn { "kotlin-logger warning: $message" }
+        customLogger.warn { "custom-logger warning: $message" }
+    }
+}
