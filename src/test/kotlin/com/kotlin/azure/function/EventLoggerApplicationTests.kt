1078e69e037305f3f6afa392a9783d394917800d
Oliver Morales Lopez <67435562+Oliver-CI@users.noreply.github.com>
2024-11-25 11:20:37 +0100

    define azure application insights logback appender

--- src/test/kotlin/com/kotlin/azure/function/EventLoggerApplicationTests.kt
+++ src/test/kotlin/com/kotlin/azure/function/EventLoggerApplicationTests.kt
@@ -3,7 +3,7 @@ package com.kotlin.azure.function
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(properties = ["APPINSIGHTS_INSTRUMENTATIONKEY=my-key"])
 class EventLoggerApplicationTests {
 
 	@Test
