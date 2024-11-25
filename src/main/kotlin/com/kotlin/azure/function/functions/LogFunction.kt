49df1c56c742c950ace83f08df59aff26bc7e7be
Oliver Morales Lopez <67435562+Oliver-CI@users.noreply.github.com>
2024-11-25 14:25:34 +0100

    cleanup

--- src/main/kotlin/com/kotlin/azure/function/functions/LogFunction.kt
+++ src/main/kotlin/com/kotlin/azure/function/functions/LogFunction.kt
@@ -1,15 +1,18 @@
 package com.kotlin.azure.function.functions
 
-import com.kotlin.azure.function.logger.LoggerFactory
 import com.microsoft.azure.functions.ExecutionContext
 import com.microsoft.azure.functions.HttpMethod
 import com.microsoft.azure.functions.HttpRequestMessage
-import com.microsoft.azure.functions.annotation.*
+import com.microsoft.azure.functions.annotation.AuthorizationLevel
+import com.microsoft.azure.functions.annotation.FunctionName
+import com.microsoft.azure.functions.annotation.HttpTrigger
 import mu.KotlinLogging
 import mu.withLoggingContext
 import org.springframework.stereotype.Component
 
 val kLogger = KotlinLogging.logger {}
+
+private const val MDC_KEY = "custom-attribute"
 
 @Component
 class LogFunction {
@@ -26,48 +29,16 @@ class LogFunction {
     ) {
         val message = request.body
         val logger = java.util.logging.Logger.getLogger(this::class.java.name)
-        logger.fine("java logger fine: $message")
-        logger.info("java logger info: $message")
-        logger.warning("java logger warn: $message")
-        logger.severe("java logger error: $message")
-    }
-
-    @FunctionName("ContextLogger")
-    fun handleContextLogger(
-        @HttpTrigger(
-            name = "request",
-            methods = [HttpMethod.POST],
-            authLevel = AuthorizationLevel.ANONYMOUS,
-            route = "context-logger"
-        ) request: HttpRequestMessage<String>,
-        context: ExecutionContext
-    ) {
-        val message = request.body
-        context.logger.fine("CONTEXT LOGGER fine: $message")
-        context.logger.info { "CONTEXT LOGGER info: $message" }
-        context.logger.warning("CONTEXT LOGGER warning: $message")
-        context.logger.severe("CONTEXT LOGGER severe: $message")
-    }
-
-    @FunctionName("ContextLoggerWithDimensions")
-    fun handleContextLoggerWithDimensions(
-        @HttpTrigger(
-            name = "request",
-            methods = [HttpMethod.POST],
-            authLevel = AuthorizationLevel.ANONYMOUS,
-            route = "context-logger-dimensions"
-        ) request: HttpRequestMessage<String>,
-        context: ExecutionContext
-    ) {
-        val message = request.body
-        context.traceContext.attributes["custom-attribute"] = "custom-value"
-        context.logger.info { "CONTEXT LOGGER traceState: ${context.traceContext.tracestate}" }
-        context.logger.fine("CONTEXT LOGGER fine: $message")
-        context.logger.info { "CONTEXT LOGGER info: $message" }
-        context.logger.warning("CONTEXT LOGGER warning: $message")
-        context.logger.severe("CONTEXT LOGGER severe: $message")
-        // map is immutable
-        context.traceContext.attributes["custom-attribute"] = "custom-value"
+        withLoggingContext(
+            mapOf(
+                MDC_KEY to "java-logger"
+            )
+        ) {
+            logger.fine("java logger fine: $message")
+            logger.info("java logger info: $message")
+            logger.warning("java logger warn: $message")
+            logger.severe("java logger error: $message")
+        }
     }
 
     @FunctionName("KotlinLogger")
@@ -81,41 +52,40 @@ class LogFunction {
         context: ExecutionContext
     ) {
         val message = request.body
-        kLogger.trace { "k-logger trace: $message" }
-        kLogger.debug { "k-logger debug: $message" }
-        kLogger.info { "k-logger info: $message" }
-        kLogger.warn { "k-logger warn: $message" }
-        kLogger.error { "k-logger error: $message" }
+        withLoggingContext(
+            mapOf(
+                MDC_KEY to "kotlin-logger"
+            )
+        ) {
+            kLogger.trace { "k-logger trace: $message" }
+            kLogger.debug { "k-logger debug: $message" }
+            kLogger.info { "k-logger info: $message" }
+            kLogger.warn { "k-logger warn: $message" }
+            kLogger.error { "k-logger error: $message" }
+        }
     }
 
-    @FunctionName("KotlinLoggerWithDimensions")
-    fun handleKotlinLoggerWithDimensions(
+
+    @FunctionName("ContextLoggerWithDimensions")
+    fun handleContextLoggerWithDimensions(
         @HttpTrigger(
             name = "request",
             methods = [HttpMethod.POST],
             authLevel = AuthorizationLevel.ANONYMOUS,
-            route = "kotlin-logger-dimensions"
+            route = "context-logger"
         ) request: HttpRequestMessage<String>,
         context: ExecutionContext
     ) {
         val message = request.body
-        kLogger.atTrace().addKeyValue("trace-dimension", "tracing").log { "k-logger-dimension trace: $message" }
-        kLogger.atDebug().addKeyValue("debug-dimension", "debugging").log { "k-logger-dimension debug: $message" }
-        kLogger.atInfo().addKeyValue("info-dimension", "informational").log { "k-logger-dimension info: $message" }
-        kLogger.atWarn().addKeyValue("warn-dimension", "warned").log { "k-logger-dimension warn: $message" }
-        kLogger.atError().addKeyValue("error-dimension", "erroneous").log { "k-logger-dimension error: $message" }
         withLoggingContext(
             mapOf(
-                "custom-attribute" to "custom-value"
+                MDC_KEY to "context-logger"
             )
         ) {
-            kLogger.trace { "k-logger-dimension trace : $message" }
-            kLogger.debug { "k-logger-dimension debug : $message" }
-            kLogger.info { "k-logger-dimension info : $message" }
-            kLogger.warn { "k-logger-dimension warn : $message" }
-            kLogger.error { "k-logger-dimension error : $message" }
+            context.logger.fine("CONTEXT LOGGER fine: $message")
+            context.logger.info { "CONTEXT LOGGER info: $message" }
+            context.logger.warning("CONTEXT LOGGER warning: $message")
+            context.logger.severe("CONTEXT LOGGER severe: $message")
         }
-
     }
-
 }
